5a6d250f694775f67c181c857c7d0248705308dd
GitHubDebaA <bapi98deba@gmail.com>
2025-01-31 23:49:07 +0530

    Changing Explore button icon

--- src/Utilities/Carousel/Carousel.js
+++ src/Utilities/Carousel/Carousel.js
@@ -5,7 +5,6 @@ import slide2 from './slide2.svg';
 import slide3 from './slide3.svg';
 import slide4 from './slide4.svg';
 import { Carousel, Container, Row, Col, Button } from 'react-bootstrap';
-import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
 
 const HomepageSlide = () => {
     return (
@@ -14,7 +13,7 @@ const HomepageSlide = () => {
                 <Container fluid>
                     <Row>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-image-column">
-                            <img className="d-block slide-images" src={slide1} alt="First slide"/>
+                            <img className="d-block slide-images" src={slide1} alt="First slide" />
                         </Col>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-content-column">
                             <div className="heading-container-01">
@@ -22,7 +21,9 @@ const HomepageSlide = () => {
                                 <p>A comprehensive website management strategy is pivotal for online business success. From enhancing user experience to improving search engine visibility, each aspect plays a crucial role.</p>
                                 <Button variant="outline-light">
                                     Explore
-                                    <MdOutlineKeyboardDoubleArrowRight />
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                                    </svg>
                                 </Button>
                             </div>
                         </Col>
@@ -34,14 +35,17 @@ const HomepageSlide = () => {
                 <Container fluid>
                     <Row>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-image-column">
-                            <img className="d-block slide-images" src={slide2} alt="Second slide"/>
+                            <img className="d-block slide-images" src={slide2} alt="Second slide" />
                         </Col>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-content-column">
                             <div className="heading-container-01">
                                 <h3>How Changes in Social Media Algorithms Are Influencing Content Strategy and What Marketers Need to Know to Stay Ahead</h3>
                                 <p>In today's bustling market where every digital marketing agency is trying to stand out, having consumable content is like the basic rule of the game. ...</p>
-                                <Button variant="outline-light">Explore
-                                    <MdOutlineKeyboardDoubleArrowRight />
+                                <Button variant="outline-light">
+                                    Explore
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                                    </svg>
                                 </Button>
                             </div>
                         </Col>
@@ -53,14 +57,17 @@ const HomepageSlide = () => {
                 <Container fluid>
                     <Row>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-image-column">
-                            <img className="d-block slide-images" src={slide3} alt="Third slide"/>
+                            <img className="d-block slide-images" src={slide3} alt="Third slide" />
                         </Col>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-content-column">
                             <div className="heading-container-01">
                                 <h3>How Artificial Intelligence is Revolutionizing Advertising, From Personalized Targeting to Predictive Analytics</h3>
                                 <p>Welcome to the future of advertising, where the magic of Artificial Intelligence (AI) is transforming the way, we connect with audiences.</p>
-                                <Button variant="outline-light">Explore
-                                    <MdOutlineKeyboardDoubleArrowRight />
+                                <Button variant="outline-light">
+                                    Explore
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                                    </svg>
                                 </Button>
                             </div>
                         </Col>
@@ -72,14 +79,17 @@ const HomepageSlide = () => {
                 <Container fluid>
                     <Row>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-image-column">
-                            <img className="d-block slide-images" src={slide4} alt="Fourth slide"/>
+                            <img className="d-block slide-images" src={slide4} alt="Fourth slide" />
                         </Col>
                         <Col xs="12" sm="12" md="6" lg="6" xl="6" className="slide-content-column">
                             <div className="heading-container-01">
                                 <h3>4 Red Flags to Watch Out in Your Next Content Marketing Strategy</h3>
                                 <p>Discover the top 4 red flags that could be undermining your content marketing strategy. Learn how to identify and address these warning signs to ensure your next campaign is a resounding success.</p>
-                                <Button variant="outline-light">Explore
-                                    <MdOutlineKeyboardDoubleArrowRight />
+                                <Button variant="outline-light">
+                                    Explore
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                                    </svg>
                                 </Button>
                             </div>
                         </Col>
