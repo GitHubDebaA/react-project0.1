8a533e6019cc7210ff7236599975cc838bff9fd6
GitHubDebaA <bapi98deba@gmail.com>
2025-01-31 22:39:40 +0530

    Adding text appearance for 2nd page.

--- src/Utilities/TileCard/TileCard.js
+++ src/Utilities/TileCard/TileCard.js
@@ -7,7 +7,7 @@ const TileCard = ({ heading, message, illustration }) => {
             {illustration ? illustration : null}
             <p class="text-body_medium project-waving-text tile-card__title">{heading}</p>
             <div className="tile-card__content">
-                <p className="text-body_medium" style={{fontWeight: 700}}>{heading}</p>
+                <p className="text-body_medium project-waving-text" style={{fontWeight: 700}}>{heading}</p>
                 <p className="tile-card__description text-body_small text-color_light">{message}</p>
             </div>
         </div>
